26a953b2a01535fef1d8df1f63482a7ad30da5f7
tivrusky <kallelr@outlook.com>
2023-01-25 16:15:15 -0300

    testando heroku

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ require("dotenv/config");
 module.exports = {
   dialect: "postgres",
   url: process.env.DATABASE_URL,
+  ssl: process.env.SSL,
   // host: process.env.DB_HOST,
   // username: process.env.DB_USER,
   // password: process.env.DB_PASS,
