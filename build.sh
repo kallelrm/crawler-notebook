990d371b7e99a78443cdc583b0da059752831c5a
tivrusky <kallelr@outlook.com>
2023-01-25 01:29:25 -0300

    fix(build): adicionando novos comandos no script de build

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ workdir=`pwd`;
 cd ~;
 curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh;
 sudo bash nodesource_setup.sh;
-sudo apt install nodejs;
+sudo apt-get install -y nodejs;
+sudo apt-get install gcc g++ make;
 
 node -v;
 
-cd $workdir
+cd $workdir;
 
 docker run --name postgres -e POSTGRES_PASSWORD=123 -e POSTGRES_USER=postgres -p 5433:5433 -d postgres;
 
