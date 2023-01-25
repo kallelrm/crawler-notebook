643a8e4b56df7f7fb61a98bbe632cf2bf71ea27b
tivrusky <kallelr@outlook.com>
2023-01-25 16:28:18 -0300

    alterando ssl

--- src/config/database.js
+++ src/config/database.js
@@ -3,16 +3,14 @@ require("dotenv/config");
 module.exports = {
   dialect: "postgres",
   url: process.env.DATABASE_URL,
-  ssl: process.env.SSL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   // host: process.env.DB_HOST,
   // username: process.env.DB_USER,
   // password: process.env.DB_PASS,
   // database: process.env.DB_NAME,
   define: {
-    ssl: {
-      require: process.env.SSL,
-      rejectUnauthorized: false,
-    },
     timestamps: true,
     underscored: true,
     underscoredAll: true,
