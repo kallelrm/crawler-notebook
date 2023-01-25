b1ab017c90fcda6e2b88ea2556e73aa75701a3c5
tivrusky <kallelr@outlook.com>
2023-01-25 18:53:30 -0300

    a

--- src/database/index.js
+++ src/database/index.js
@@ -14,7 +14,8 @@ class Database {
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
+    this.connection = new Sequelize(process.env.DATABASE_URL, { ...databaseConfig });
+    console.log(this.connection);
 
     models
       .map((model) => model.init(this.connection))
